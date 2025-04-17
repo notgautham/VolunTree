ad997646ebd626ecbe14684706d2944a2e921744
notgautham <vgautham.coll@gmail.com>
2025-04-17 13:28:36 +0530

    updated navbar

--- code/src/components/Navbar.jsx
+++ code/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import picture2 from "../assets/picture2.png";
+/*import picture2 from "../assets/picture2.png";*/
 
 const NavbarContainer = styled.nav`
   background-color: #ffffff;
